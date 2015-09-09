15d751a0cd01bf713bb69869d3220ff5ee7357ee
AIDAN PARTINGTON <AidanPartington@gmail.com>
2015-09-09 10:27:56 +1000

    removed model

--- app/models/conference_info.rb
+++
@@ -1,2 +0,0 @@
-class ConferenceInfo < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -27,16 +27,6 @@ ActiveRecord::Schema.define(version: 20150908045102) do
     t.string   "twitterhandle"
   end
 
-  create_table "conference_infos", force: :cascade do |t|
-    t.string   "name"
-    t.date     "date"
-    t.time     "time"
-    t.text     "venue"
-    t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "speakers", force: :cascade do |t|
     t.string   "name"
     t.string   "background"
